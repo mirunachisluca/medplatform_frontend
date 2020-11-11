32b0010dd7704b6269941c663525546baa7c3647
Miruna Chisluca <68232365+miruna-chisluca@users.noreply.github.com>
2020-11-11 18:01:01 +0200

    assignment 2

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://medplatformapi.herokuapp.com",
+  baseURL: "https://medplatformapi2.herokuapp.com/",
 });
 
 export { axiosInstance };
--- src/components/Caregiver/CaregiverPatients.js
+++ src/components/Caregiver/CaregiverPatients.js
@@ -4,13 +4,49 @@ import { DataTable } from "../Tables/DataTable";
 import { patientsHeader } from "../tableHeaders";
 import { axiosInstance } from "../../api/axios";
 import AuthContext from "../../context/context";
+import { HubConnectionBuilder } from "@microsoft/signalr";
+import { useSnackbar } from "notistack";
 
 function CaregiverPatients() {
+  const [connection, setConnection] = React.useState(null);
   const { user } = React.useContext(AuthContext);
   const [patientData, setPatientData] = React.useState({
     isLoaded: false,
     data: null,
   });
+  const { enqueueSnackbar } = useSnackbar();
+
+  React.useEffect(() => {
+    const newConnection = new HubConnectionBuilder()
+      .withUrl("https://medplatformapi2.herokuapp.com/hubs/activity")
+      .withAutomaticReconnect()
+      .build();
+    setConnection(newConnection);
+  }, []);
+
+  React.useEffect(() => {
+    if (connection) {
+      connection
+        .start()
+        .then((result) => {
+          connection.on("SendMessage", (response) => {
+            console.log("============> MESSAGE: ", response);
+            var message = response.patientName + " " + response.message;
+            if (parseInt(response.caregiverId) === user.id) {
+              enqueueSnackbar(message, {
+                variant: "warning",
+                anchorOrigin: {
+                  vertical: "bottom",
+                  horizontal: "right",
+                },
+                autoHideDuration: 5000,
+              });
+            }
+          });
+        })
+        .catch((e) => console.log("===========> Connection failed. ", e));
+    }
+  }, [connection]);
 
   React.useEffect(() => {
     setPatientData({ isLoaded: false });
